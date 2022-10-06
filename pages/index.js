471ae05a622645f27bb7e37a77f116059e0317dc
colinaluke <colinalukesc@gmail.com>
2022-10-06 13:10:47 +0800

    edited index

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
               <title>{siteTitle }</title>
           </Head>
           <section className={utilStyles.headingMd}>
-              <p>Hello, I am Luke Abram Colina. A fourth year BSCS student.</p>
+              <p>Bleep.</p>
               <p>
                   (This is a sample website - you'll be building a site like this on {' '} <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
               </p>
